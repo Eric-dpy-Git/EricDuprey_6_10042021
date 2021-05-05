d46543db137acfafc9b896e986912e28ca4570c8
Eric <eric.dpy@gmail.com>
2021-05-05 15:30:00 +0200

    server.js improvements changes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,54 @@
-//http node package importation
-const https = require("https");
-
-//import application express from file app.js
+//package http import
+const http = require("http");
+//app import to use on serve
 const app = require("./app");
 
-//server creation
-const server = https.createServer(app); //pass the application to the server
+//the  normalizePort  function returns a valid port, whether it is provided as a number or a string
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-//need to tell express application on wich port it run --> process.env.PORT if 3000 default port not available
-app.set("port", process.env.PORT || 3000);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+//listen on port 3000 if no return
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
-//server have to listen request --> process.env.PORT if 3000 default port not available
-server.listen(process.env.PORT || 3000);
+//errorHandler search, manage and save error on server
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+//create server with express (need to be install with terminal before) and and const for request/response server
+const server = http.createServer(app);
+
+//server on --> errorhandler manage & log in console
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
+//server listen on port set in const
+server.listen(port);
